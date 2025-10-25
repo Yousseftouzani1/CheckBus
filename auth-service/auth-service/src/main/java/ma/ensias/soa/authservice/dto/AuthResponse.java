9afc289c8489880fa8371a39b5fff617723d5be7
Younes Bazzaoui <younrun.rocka@gmail.com>
2025-10-25 01:08:46 +0100

    added some variables and cleaned the code a little

--- auth-service/auth-service/src/main/java/ma/ensias/soa/authservice/dto/AuthResponse.java
+++ auth-service/auth-service/src/main/java/ma/ensias/soa/authservice/dto/AuthResponse.java
@@ -1,8 +1,6 @@
 package ma.ensias.soa.authservice.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- auth-service/auth-service/src/main/java/ma/ensias/soa/authservice/dto/ResendVerificationRequest.java
+++ auth-service/auth-service/src/main/java/ma/ensias/soa/authservice/dto/ResendVerificationRequest.java
@@ -2,7 +2,6 @@ package ma.ensias.soa.authservice.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
